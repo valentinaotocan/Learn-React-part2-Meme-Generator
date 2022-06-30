649067195c7e2b51047c47cc75ff4f0d693f136a
Valentina Otočan <valentinaotocan@gmail.com>
2022-06-30 11:33:06 +0200

    Setup, jsx elements and styles

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header className="header">
+      <img src="../images/logo.svg" alt="Logo" />
+      <h2 className="heading-course">React course - part 2</h2>
+    </header>
+  );
+}
---
+++ src/components/Meme.js
@@ -0,0 +1,13 @@
+export default function Meme() {
+  return (
+    <main>
+      <form className="form">
+        <input type="text" className="form-input" />
+        <input type="text" className="form-input" />
+        <button className="form-button">
+          Get a new meme image 🖼
+        </button>
+      </form>
+    </main>
+  );
+}
